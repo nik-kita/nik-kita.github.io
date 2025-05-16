f202c77320dd72022df119367639dcfc191963ff
nik-kita <nik@kita.github>
2025-05-16 22:16:19 +0200

    update _cms.ts config file, add default github deployment.yml

--- _cms.ts
+++ _cms.ts
@@ -1,3 +1,15 @@
+import { load } from "@std/dotenv";
 import cms from "blog/_cms.ts";
+
+const { CMS_ADMIN_PASSWORD, RUNTIME_ENV } = await load();
+
+if (!CMS_ADMIN_PASSWORD) {
+  throw new Deno.errors.NotFound("cms config should be loaded!");
+}
+
+cms.auth({
+  admin: CMS_ADMIN_PASSWORD,
+  ...(RUNTIME_ENV !== "production" && { "": "" }),
+});
 
 export default cms;
